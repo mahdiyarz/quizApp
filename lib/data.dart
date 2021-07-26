a7dcb8401f904572e414700e34ebfc550efff8ba
mahdiyarz <m.arbabzi@gmail.com>
2021-07-26 09:56:11 -0700

    create data for my app

---
+++ lib/data.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import './models/app_model.dart';
+
+var ourData = [
+  AppModel(
+    id: 'q1',
+    question: 'What\'s my best FUCKING friend?',
+    answers: [
+      {'textAns': 'Sinaz', 'score': 50},
+      {'textAns': 'Vahab', 'score': 10},
+      {'textAns': 'Sajad', 'score': 30},
+    ],
+  ),
+  AppModel(
+    id: 'q2',
+    question: 'What was my best FUCKING car?',
+    answers: [
+      {'textAns': 'P.K.', 'score': 10},
+      {'textAns': 'Peykan', 'score': 5},
+      {'textAns': 'Pars ELX', 'score': 30},
+      {'textAns': 'Pride 111', 'score': 20},
+      {'textAns': 'MVM 530', 'score': 50},
+    ],
+  ),
+  AppModel(
+    id: 'q3',
+    question: 'What was my best FUCKING mobile?',
+    answers: [
+      {'textAns': 'Samsung Note3', 'score': 10},
+      {'textAns': 'Nokia 6021', 'score': 5},
+      {'textAns': 'Xiaomi Note8 Pro', 'score': 50},
+      {'textAns': 'Nokia 3250', 'score': 20},
+    ],
+  )
+];
